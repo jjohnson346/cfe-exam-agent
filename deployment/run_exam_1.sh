42e52c08079339365ee5e4eb160564ec5f5582af
Joe Johnson <jjohnson346@gmail.com>
2015-11-20 18:09:37 -0500

    modified run_exam_1.sh to use file encoding of iso-8859-1 (from utf-8).

--- deployment/run_exam_1.sh
+++ deployment/run_exam_1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp cfe-exam-agent-@version@.jar -Xms128m -Xmx4096m financial.fraud.cfe.agent.CFEExamAgent
+java -Dfile.encoding=ISO-8859-1 -cp cfe-exam-agent-@version@.jar -Xms128m -Xmx4096m financial.fraud.cfe.agent.CFEExamAgent
 
